2d97955df9f40f56774c445e46d7365f243b5227
Chris Booth <31346759+chrisPhat@users.noreply.github.com>
2021-11-08 16:38:03 +0000

    sorting mess

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,39 @@
+let mySong, amplitude;
+
+function preload(){
+    mySong = loadSound('assets/massiveAttackAngel.mp3');
+}
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(window.innerWidth, window.innerHeight);
     canvas.parent("p5container");
+
+    mySong.play();
+
+    amplitude = new p5.Amplitude();
 }
 
 function draw() {
-    background(220);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    let level = amplitude.getLevel();
+
+    let vol = map(level, 0, 1, 0, 1000);
+    let col = map(level, 0, 1, 0, 255);
+    //console.log(col);
+
+    background(col);
+
+    noStroke();
+
+    if (vol >= 200){
+        fill(col + 10);
+        ellipse(width / 2, height / 2, vol + 100);
+    }
+
+    fill(col, 100, 100);
+    ellipse(width/2, height/2, vol);
+
+    fill(100, col, 100);
+    ellipse(width/2, height/2, vol/2);
+    
 }
